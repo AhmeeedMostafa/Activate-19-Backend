d7310fbafb2920872aac872827305beb96caea6d
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2019-10-11 18:21:24 +0200

    Securely utilizing sheets queries.

--- src/queries/sheetsQueries.js
+++ src/queries/sheetsQueries.js
@@ -1,7 +1,18 @@
 const GoogleSpreadSheet = require('google-spreadsheet');
 const { promisify } = require('util');
 
-const creds = require('../../client_secret.json');
+const creds = {
+    "type": process.env.type,
+    "project_id": process.env.project_id,
+    "private_key_id": process.env.private_key,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": process.env.auth_uri,
+    "token_uri": process.env.token_uri,
+    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.client_x509_cert_url
+}
 
 const chooseWorksheet = (worksheets, lc) => {
     let n = -1;

a602e0a979b1bc33eabda2c3fb203921c79299a7
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2019-10-11 19:41:42 +0200

    securing the 3rd parties

--- app.js
+++ app.js
@@ -14,29 +14,6 @@ const orders = require('./src/routes/orders');
 
 const { success } = require('./src/assets/responses');
 
-// const creds = JSON.stringify({
-//   type: process.env.type,
-//   project_id: process.env.project_id,
-//   private_key_id: process.env.private_key,
-//   private_key: process.env.private_key,
-//   client_email: process.env.client_email,
-//   client_id: process.env.client_id,
-//   auth_uri: process.env.auth_uri,
-//   token_uri: process.env.token_uri,
-//   auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-//   client_x509_cert_url: process.env.client_x509_cert_url
-// });
-
-// console.log(creds);
-
-// const fs = require('fs');
-
-// fs.writeFile('client_secret.json', creds, function (err) {
-//   if (err) throw err;
-//   console.log('Saved!');
-// .
-// 0});
-
 const app = express();
 const port = process.env.PORT || '3000';
 
